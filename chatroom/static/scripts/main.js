30b9a794f2a3fcddf6218db4f3f5e63343103a9b
zeizyy <yz9fy@virginia.edu>
2016-07-26 16:41:28 -0700

    get messages

--- chatroom/static/scripts/main.js
+++ chatroom/static/scripts/main.js
@@ -20,9 +20,6 @@
     
 // }
 
-$(function() {
-
-    var position;
 
     function getLocation(){
         if (navigator.geolocation){
@@ -60,6 +57,43 @@ $(function() {
             }
         });
     };
+
+    function get_messages() {
+                $.ajax({
+                    type: "GET",
+                    url: 'http://127.0.0.1:8000/chatroom/get/',
+                    dataType: 'json',
+                    // Process the data
+                    success: function(responseObject) {
+                        // Display the JSON query status
+                        document.getElementById("status").innerHTML = "Status: " + responseObject.status;
+                        /* Put the elements back to the DOM */
+                        var messages = document.getElementById("response");
+                        while(messages.firstChild) {
+                            messages.removeChild(messages.firstChild);
+                        }
+                        // Display the JSON query messages
+                        for(i = 0; i < responseObject.resp.length; i++) {
+                            // Create an entry for the message
+                            var messageContentP = document.createElement("p");  // <p>
+                            var messageContentText = document.createTextNode("Message " + i.toString() + ": " + responseObject.resp[i][0]); // Get the text to display
+                            /* Put the text into the <p> element */
+                            messageContentP.appendChild(messageContentText);
+                            var messageType = responseObject.resp[i][1];
+                            /* Set the color based on the message type */
+                            if(messageType == "n") {
+                                messageContentP.style.color = "green";
+                            } else {
+                                messageContentP.style.color = "grey";
+                            }
+                            messages.appendChild(messageContentP);
+                        }
+                        /* Get the current timestamp */
+                        var timestamp = Date.now();
+                        document.getElementById("timestamp").innerHTML = timestamp.toString();
+                    }
+                });
+            };
 
 
     // This function gets cookie with a given name
@@ -112,5 +146,3 @@ $(function() {
             }
         }
     });
-
-});
