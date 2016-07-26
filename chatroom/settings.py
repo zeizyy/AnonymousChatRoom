961d2a620cc357bbc0e7f0c333f109cf11983a9a
zeizyy <yz9fy@virginia.edu>
2016-07-26 10:41:36 -0700

    database

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'chatroom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chat',
+        'USER': 'chatadmin',
+        'PASSWORD': 'microsoft',
+        'HOST': 'db',
     }
 }
 
