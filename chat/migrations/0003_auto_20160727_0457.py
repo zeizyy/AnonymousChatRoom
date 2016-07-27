a180a4408f57e8cda331563dcf0e7c1e9e1de97e
zeizyy <yz9fy@virginia.edu>
2016-07-26 21:58:55 -0700

    null

---
+++ chat/migrations/0003_auto_20160727_0457.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('chat', '0002_auto_20160726_1720'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='chat.User', null=True),
+        ),
+    ]
